af93d59ce11f66a7db39d2d3ba3e2b0457091bcb
Matthew Parker <matthewparker@hotmail.com.au>
2022-03-20 15:53:04 +1000

    initial XCode Commit

---
+++ FreeTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FreeTime
+//
+//  Created by Matthew Parker on 20/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FreeTime/FreeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeTimeApp.swift
+//  FreeTime
+//
+//  Created by Matthew Parker on 20/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FreeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
